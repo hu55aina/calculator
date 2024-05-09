54e08b03f6817ca96ec22756fb74513146f699c6
hu55aina <ihussaina910@gmail.com>
2024-05-09 18:54:22 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+// calculator in typecript using the terminal input
+Object.defineProperty(exports, "__esModule", { value: true });
+var prompt = require("prompt-sync")();
+var n1 = parseInt(prompt("enter your first number:"));
+var n2 = parseInt(prompt("enter your second number:"));
+var operator = (prompt("enter operator(+,-,*,/):"));
+if (operator = "+") {
+    console.log("the answer of ".concat(n1, " ").concat(operator, " ").concat(n2, " = ").concat(n1 + n2));
+}
+if (operator = "-") {
+    console.log("the answer of ".concat(n1, " ").concat(operator, " ").concat(n2, " = ").concat(n1 - n2));
+}
+if (operator = "*") {
+    console.log("the answer of ".concat(n1, " ").concat(operator, " ").concat(n2, " = ").concat(n1 * n2));
+}
+if (operator = "/") {
+    console.log("the answer of ".concat(n1, " ").concat(operator, " ").concat(n2, " = ").concat(n1 / n2));
+}
+else {
+    console.log("please select correct operator");
+}
